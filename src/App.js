46f3dcd1861bb01191a6c74af2cd98bc9266b914
elizhang227 <zhang.eli@hotmail.com>
2019-06-28 14:31:38 -0400

    created 3 buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import ElapsedTime from './elapsedTime';
+
 
 function App() {
   return (
     <div className="App">
-
+      <ElapsedTime />
     </div>
   );
 }
--- src/elapsedTime.js
+++ src/elapsedTime.js
@@ -3,6 +3,22 @@ import PropTypes from 'prop-types';
 
 class ElapsedTime extends Component {
 
+
+    render() {
+        let i = 0
+        return(
+            <div>
+                <p>0ms</p> 
+                {/* {setInterval(() => {
+                    return i = i + 1;
+                }, 1000)} */}
+                <button>start</button>
+                <button>stop</button>
+                <button>reset</button>
+            </div>
+        )
+    }
+
 }
 
 export default ElapsedTime;
